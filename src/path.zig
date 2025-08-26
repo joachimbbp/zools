d736c77530972315c6b40132dcea83587830ca10
Joachim Pfefferkorn <104856283+joachimbbp@users.noreply.github.com>
2025-08-25 20:45:56 -0400

    File versioning (#1)
    * addVersion works
    
    * tests pass
    
    * test.zig tests pass in zig 0.15.1
    
    * iteration test works in file.zig
    
    * fixing git gore
    
    * versioning works for non cwd() stuff
    
    * ls added, testing still needs work
    
    * first version issue fixed
    
    * tests pass

--- src/path.zig
+++ src/path.zig
@@ -1,9 +1,92 @@
 const std = @import("std");
 const iter = @import("iter.zig");
+const string = @import("string.zig");
+const debug = @import("debug.zig");
+const print = std.debug.print;
+const ArrayList = std.array_list.Managed;
 
-pub fn exists(path_string: []const u8) bool {
-    _ = std.fs.cwd().openFile(path_string, .{}) catch {
+const PathError = error{
+    PathDoesNotExist,
+    ExtensionError,
+};
+
+pub fn exists(path: []const u8) bool {
+    _ = std.fs.cwd().openFile(path, .{}) catch {
+        //WARNING: blunt instrument, not fully covered, clean this guy up!
         return false;
     };
     return true;
+}
+
+// Returns true only if the path is to a folder
+//TODO: isDir function
+//Separate basename function would be used many places
+
+// Lists all the files in a directory
+pub fn ls(path: []const u8, alloc: std.mem.Allocator) !ArrayList([]u8) {
+    if (!exists(path)) {
+        return PathError.PathDoesNotExist;
+    }
+    var dir = try std.fs.cwd().openDir(path, .{ .iterate = true });
+    defer dir.close();
+    var output = ArrayList([]u8).init(alloc);
+
+    var walker = try dir.walk(alloc);
+    defer walker.deinit();
+
+    while (try walker.next()) |entry| {
+        const abs = try entry.dir.realpathAlloc(alloc, entry.path);
+
+        try output.append(abs);
+    }
+    return output;
+}
+
+pub fn addVersion(filepath: []const u8, alloc: std.mem.Allocator) !ArrayList(u8) {
+    const v_sep = "_";
+    const f_pattern = "{s}/{s}_{d}.{s}";
+    var output = ArrayList(u8).init(alloc);
+
+    if (!exists(filepath)) {
+        print("path does not exist: {s}\n \n", .{filepath});
+        for (filepath) |c| {
+            try output.append(c);
+        }
+        return output;
+    }
+    const folders = try string.split(filepath, "/", alloc);
+    defer folders.deinit();
+    const directory: []const u8 = try std.mem.join(alloc, "/", folders.items[0 .. folders.items.len - 1]);
+    defer alloc.free(directory);
+    const filename = folders.items[folders.items.len - 1];
+    const filename_split = try string.split(filename, ".", alloc);
+    defer filename_split.deinit();
+    if (filename_split.items.len != 2) {
+        return PathError.ExtensionError;
+    }
+    const basename = filename_split.items[0];
+    const extension = filename_split.items[1];
+
+    //filebasename ... any underscores ... version number
+    const v_split = try string.split(basename, v_sep, alloc);
+    defer v_split.deinit();
+
+    var prefix_chop: u1 = 0;
+    const suffix = v_split.items[v_split.items.len - 1];
+    var version: u32 = 1;
+
+    if (string.isInteger(suffix)) {
+        version = try std.fmt.parseInt(u32, suffix, 10) + 1;
+        prefix_chop = 1;
+    } else {}
+    const prefix = try std.mem.join(alloc, v_sep, v_split.items[0 .. v_split.items.len - prefix_chop]);
+    defer alloc.free(prefix);
+
+    const result = try std.fmt.allocPrint(alloc, f_pattern, .{ directory, prefix, version, extension });
+    defer alloc.free(result);
+
+    for (result) |c| {
+        try output.append(c);
+    }
+    return output;
 }
--- src/test.zig
+++ src/test.zig
@@ -1,21 +1,26 @@
-//Last successful run was zig version 0.14.1
+//NOTE: must run from the project root, not src
+//should run as: <zig test src/test.zig>
 
 const std = @import("std");
 const expect = std.testing.expect;
 const print = std.debug.print;
+const sleep = std.Thread.sleep;
 
 const iter = @import("iter.zig");
 const path = @import("path.zig");
 const dir = @import("dir.zig");
-const file = @import("file.zig");
+//const file = @import("file.zig");
 const string = @import("string.zig");
 
 const one_sec: u64 = 1 * std.time.ns_per_s;
 
+const test_dir = "./test_files";
+
 test "path exists" {
     //TODO: build custom folders so this doesn't need to run from the root
     try expect(!path.exists("hd998e9db-f335-4499-91e0-cb941fdeed3/home"));
-    try expect(path.exists("./test.zig"));
+    try expect(path.exists("./src/test.zig"));
+    try expect(path.exists(test_dir));
 }
 
 test "build directory if absent" {
@@ -23,7 +28,7 @@ test "build directory if absent" {
     const temp_folder = "./temp";
     try dir.buildIfAbsent(temp_folder);
     try expect(path.exists(temp_folder));
-    std.time.sleep(one_sec);
+    sleep(one_sec);
     try std.fs.cwd().deleteTree(temp_folder);
 }
 
@@ -39,7 +44,56 @@ test "splitting" {
     try std.testing.expect(std.mem.eql(u8, csv_list.items[10], "10"));
 }
 test "is number" {
-    try expect(string.isNumber("42"));
-    try expect(string.isNumber("0"));
-    try expect(!string.isNumber("ham"));
+    try expect(string.isInteger("42"));
+    try expect(string.isInteger("0"));
+    try expect(!string.isInteger("ham"));
 }
+
+test "ls" {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    var arena = std.heap.ArenaAllocator.init(allocator);
+    defer arena.deinit();
+    const a = arena.allocator();
+
+    const list = try path.ls("/Users/joachimpfefferkorn/repos/zools/test_files", a);
+    defer list.deinit();
+    for (0..list.items.len) |n| {
+        print("item: {s}\n", .{list.items[n]});
+    }
+
+    // const item = try path.ls("/Users/joachimpfefferkorn/repos/zools/test_files/ham.txt", a);
+    // defer item.deinit();
+    // print("item: {s}\n", .{item.items[0]});
+}
+test "versioning" {
+    //NOTE: this test particularly could use a lot of work
+
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const alloc = gpa.allocator();
+    defer _ = gpa.deinit();
+    //probably need to run from root, not src
+
+    const version_me = "/Users/joachimpfefferkorn/Desktop/v_10.txt";
+    const versioned = try path.addVersion(version_me, alloc);
+    defer versioned.deinit();
+    print("versioned: {s}\n \n \n", .{versioned.items});
+
+    const first_version = "/Users/joachimpfefferkorn/Desktop/ham.txt";
+    const first_versioned = try path.addVersion(first_version, alloc);
+    defer first_versioned.deinit();
+    print("first versioned: {s}\n \n \n", .{first_versioned.items});
+
+    const version_with_underscore = "/Users/joachimpfefferkorn/Desktop/v_with_under_59.txt";
+    const versioned_alt = try path.addVersion(version_with_underscore, alloc);
+    defer versioned_alt.deinit();
+    print("alt: {s}\n \n \n", .{versioned_alt.items});
+
+    // const ds_store = "/Users/joachimpfefferkorn/Desktop/.Ds_store";
+    // const ds_stored = try addVersion(ds_store, alloc);
+    // defer ds_stored.deinit();
+    // print("ds store test {s}", .{ds_stored.items});
+}
+//TODO: build test files programatically
