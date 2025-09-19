15b125a101ebb9b3d39902dc57515ed47a78f376
joachimbbp <104856283+joachimbbp@users.noreply.github.com>
2025-09-19 16:41:38 -0400

    timer added to root

--- src/root.zig
+++ src/root.zig
@@ -4,3 +4,4 @@ pub const string = @import("string.zig");
 pub const path = @import("path.zig");
 pub const save = @import("save.zig");
 pub const uuid = @import("uuid.zig");
+pub const timer = @import("timer.zig");
