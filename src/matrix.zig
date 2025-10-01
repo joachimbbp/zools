9853dcb3ea978c9cd7a362e253b0d6f2cdeae216
joachimbbp <104856283+joachimbbp@users.noreply.github.com>
2025-10-01 16:42:12 -0400

    identity matrix constant added

---
+++ src/matrix.zig
@@ -0,0 +1,9 @@
+//eventually this might hold a bunch of matrix transformation
+//functions. For now it's just an identity constant.
+
+const IdentityMatrix4x4: [4][4]f64 = .{
+    .{ 1.0, 0.0, 0.0, 0.0 },
+    .{ 0.0, 1.0, 0.0, 0.0 },
+    .{ 0.0, 0.0, 1.0, 0.0 },
+    .{ 0.0, 0.0, 0.0, 1.0 },
+};
