419d694f77d47ef705524dc7c530220989637a9f
joachimbbp <104856283+joachimbbp@users.noreply.github.com>
2025-10-02 19:18:26 -0400

    wip, structs print

---
+++ src/csv.zig
@@ -0,0 +1,69 @@
+const std = @import("std");
+
+//s is a struct
+pub fn fromStruct(s: anytype) void {
+    //reutnr will be: ![]const u8
+    const T = @TypeOf(s);
+    const info = @typeInfo(T).@"struct";
+
+    //    var csv: std.array_list.Managed(u8) = undefined;
+    inline for (info.fields) |field| {
+        const value = @field(s, field.name);
+        //debug print for now
+        //TODO: Char8s format as letters if given the option
+        std.debug.print("{s} = {any}\n", .{ field.name, value });
+        //TODO: actually append this to a csv: even is key and odd is value
+    }
+}
+
+//TESTING:
+pub const BigStruct = struct {
+    a: i32,
+    b: bool,
+    c: f64,
+    d: []const u8,
+    e: [5]u8,
+    f: ?i16,
+    g: ?[]const u8,
+    h: struct {
+        x: i32,
+        y: bool,
+    },
+    i: [3]f32,
+    j: ?struct {
+        m: u64,
+        n: []const u8,
+    },
+    k: u8,
+    l: []const u8,
+    m: bool,
+    n: [2]i32,
+    o: ?f32,
+    p: struct {
+        q: []const u8,
+        r: bool,
+    },
+};
+
+test "from struct" {
+    const big = BigStruct{
+        .a = 123,
+        .b = true,
+        .c = 3.14159,
+        .d = "hello",
+        .e = .{ 1, 2, 3, 4, 5 },
+        .f = null,
+        .g = "optional string",
+        .h = .{ .x = 99, .y = false },
+        .i = .{ 0.1, 0.2, 0.3 },
+        .j = .{ .m = 42, .n = "nested optional" },
+        .k = 255,
+        .l = "zig struct test",
+        .m = false,
+        .n = .{ 10, 20 },
+        .o = 0.5,
+        .p = .{ .q = "inner struct", .r = true },
+    };
+
+    fromStruct(big);
+}
